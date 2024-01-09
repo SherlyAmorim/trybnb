4705b8079b5b30a8778089c9f3fa42db75d5fd06
SherlyAmorim <sherlyamorim@gmail.com>
2024-01-09 18:22:16 -0300

    :construction: Req8 - populating the list, in progress

--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ReservationFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ReservationFragment.kt
@@ -1,23 +1,27 @@
 package com.betrybe.trybnb.ui.views.fragments
 
 import android.os.Bundle
-// import android.util.Log
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-// import androidx.lifecycle.lifecycleScope
-// import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import com.betrybe.trybnb.R
-// import com.betrybe.trybnb.data.api.BookingServiceClient
-// import com.betrybe.trybnb.ui.adapters.ReservationsAdapter
-// import kotlinx.coroutines.launch
+import com.betrybe.trybnb.data.api.BookingServiceClient
+import com.betrybe.trybnb.ui.adapters.ReservationsAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ReservationFragment : Fragment() {
 
-//    private val mBookingService = BookingServiceClient.instance
-//    private lateinit var mBookingAdapter: ReservationsAdapter
-//    private lateinit var recyclerView: RecyclerView
+    private lateinit var buttonReservation: BottomNavigationView
+    private val mBookingService = BookingServiceClient.instance
+    private lateinit var mBookingAdapter: ReservationsAdapter
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,25 +33,34 @@ class ReservationFragment : Fragment() {
             container,
             false
         )
-//        recyclerView = view.findViewById(R.id.reservation_recycler_view)
-//
-//        fetchReservations()
-//
-//        return view
     }
 
-//    private fun fetchReservations() {
-//        lifecycleScope.launch {
-//            val response = mBookingService.getBookingResponse()
-//            if (response.isSuccessful) {
-//                val reservations = response.body()
-//                Log.d("Reservation", reservations.toString())
-//
-//                mBookingAdapter = ReservationsAdapter(reservations)
-//                recyclerView.adapter = mBookingAdapter
-//            } else {
-//                "Erro"
-//            }
-//        }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonReservation = view.findViewById(R.id.reservation_menu_item)
+
+        buttonReservation.setOnClickListener {
+            recyclerView = view.findViewById(R.id.reservation_frame_layout)
+            fetchReservations()
+        }
+    }
+
+    private fun fetchReservations() {
+        lifecycleScope.launch {
+            val response = mBookingService.getBookingResponse()
+            if (response.isSuccessful) {
+                val reservations = response.body()
+                val responseId = reservations!!.mapNotNull {
+                    mBookingService.getBookingById(it.bookingId).body()
+                }
+                withContext(Dispatchers.Main) {
+                    mBookingAdapter = ReservationsAdapter(responseId)
+                    recyclerView.adapter = mBookingAdapter
+                }
+            } else {
+                Log.d("ResponseError", "Erro ao buscar reservas")
+            }
+        }
+    }
 }
